db230d9441cb5eca5b255eb0f46ab1005aff5d2c
ww456zz <109567046+ww456zz@users.noreply.github.com>
2022-09-14 03:25:29 -0500

    Add files via upload

---
+++ java-assignment-0-hello-world-ww456zz/src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Created by LaunchCode
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println(sayHello());
+    }
+
+    public static String sayHello() {
+        return "Hello, World!";
+    }
+
+}
